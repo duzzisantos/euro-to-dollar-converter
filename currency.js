f9cf24d7a25ce16d6423ba0ba6c24e6c8808cf96
Duzie Uche-Abba <72194611+duzzisantos@users.noreply.github.com>
2021-10-15 23:03:53 +0200

    Add files via upload

---
+++ currency.js
@@ -0,0 +1,34 @@
+(function onload() {
+    setButtonFunctions();
+
+    getExchangeRate();
+})();
+
+function setButtonFunctions() {
+    document.getElementById("buttonCurrency").onclick = getExchangeRate();
+};
+
+// Currency Exchange rates
+async function getExchangeRate() {
+    const from = document.getElementById('currencyFrom').value;
+    const to = document.getElementById('currencyTo').value;
+    await fetch("https://currency-exchange.p.rapidapi.com/exchange?q=1.0&from=" + from + "&to=" + to, { //&to to match what we have on on ?q
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "currency-exchange.p.rapidapi.com",
+            "x-rapidapi-key": "1f6aba5149msha00241a4cbf041fp12ab4ajsn0544aef4b5de"
+        }
+    })
+        .then(response => response.json())
+        .then(response => {
+            console.log("Currency Exchange API object:");
+            console.log(response);
+            console.log("\n");
+
+            // display data
+            document.getElementById('innerResult').innerHTML = 'Result: ' + response;
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
